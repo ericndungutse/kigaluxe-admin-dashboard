36e1ec8e2c05bb2676feb4b622c773e283d2b28b
ericndungutse <dav.ndungutse@gmail.com>
2024-10-09 14:15:01 +0200

    remove .env in git repo

--- src/services/locations.service.js
+++ src/services/locations.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getAllLocations = async () => {
   try {
-    const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/places`);
+    const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/place`);
 
     return response.data.data;
   } catch (error) {
