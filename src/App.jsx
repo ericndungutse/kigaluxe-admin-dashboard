00de04103e80e49cff8a9c4555920ee044c19b64
ericndungutse <dav.ndungutse@gmail.com>
2024-10-14 10:38:44 +0200

    Protect account page

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,32 @@ import Categories from './pages/Categories';
 import Users from './pages/Users';
 import Appointments from './pages/Appointments';
 import Blogs from './pages/Blogs';
+import { useUser } from './hooks/useUser';
+
+const Protect = ({ children }) => {
+  const user = useUser();
+
+  console.log(user);
+
+  if (!user.user) {
+    return <Navigate to='/' replace />;
+  }
+
+  return children;
+};
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/account' element={<AccountLayout />}>
+        <Route
+          path='/account'
+          element={
+            <Protect>
+              <AccountLayout />
+            </Protect>
+          }
+        >
           <Route index element={<Navigate replace to='properties' />} />
           <Route path='properties' element={<Properties />} />
           <Route path='locations' element={<Locations />} />
