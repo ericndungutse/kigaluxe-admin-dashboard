990cf4b0a4be58f6192068d5f64d6bfe547305cf
ericndungutse <dav.ndungutse@gmail.com>
2024-10-16 11:01:42 +0200

    fix unterminated string in location service

--- src/services/locations.service.js
+++ src/services/locations.service.js
@@ -74,7 +74,8 @@ export const uploadLocationImageApi = async (locationId, formData, token) => {
 export const searchLocation = async (query, page) => {
   try {
     const response = await axios.post(
-      `${import.meta.env.VITE_BACKEND_URL}/api/place/search?location=${query}&page=${page});
+      `${import.meta.env.VITE_BACKEND_URL}/api/place/search?location=${query}&page=${page}`
+    );
 
     return response.data.data;
   } catch (error) {
